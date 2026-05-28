cd6d23b2605957e96505f46e8d303771ef599aa8
pedro-moser <pedro.niederauer.moser@gmail.com>
2026-05-28 13:25:46 -0300

    fix: Moment's Notice full form with A section repeat before coda

--- src/theory/chart.rs
+++ src/theory/chart.rs
@@ -22,6 +22,9 @@ pub const PRESETS: &[(&str, &str)] = &[
          Dm7 G7 | Ebm7 Ab7 | Dbmaj7 | Dm7 G7 | \
          Cm7 B7 | Bbm7 Eb7 | Abmaj7 | Abm7 Db7 | \
          Gm7 C7 | Abm7 Db7 | Gbmaj7 | Fm7 Bb7 | \
+         Em7 A7 | Fm7 Bb7 | Ebmaj7 | Abm7 Db7 | \
+         Dm7 G7 | Ebm7 Ab7 | Dbmaj7 | Dm7 G7 | \
+         Cm7 B7 | Bbm7 Eb7 | Abmaj7 | Abm7 Db7 | \
          Gm7 C7 | Fm7 Bb7 | Ebmaj7 | Fm7 | \
          Gm7 | Fm7 | Ebmaj7 Fm7 | Gm7 Fm7 | \
          Ebmaj7 | %",
